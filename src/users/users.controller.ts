251887bd0f8e020296cadb632469234931e96daa
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2025-01-16 17:13:49 +0700

    Controller Api endpoint Lesson02

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() //Get /users
+  findAll(@Query('name') name?: string) {
+    return { name, data: [] };
+  }
+
+  @Get('interns') //Get /users/interns
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id') //Get /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() //Post /users
+  create(@Body() user: { name: string }) {
+    return user;
+  }
+
+  @Patch(':id') //Patch /user:
+  update(@Param('id') id: string, @Body() userUpdate: { name: string }) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //Delete /users/:id
+  delete(@Param('id') id: string) {
+    return { deletedId: id };
+  }
+}
